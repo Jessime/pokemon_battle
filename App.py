53aa3784dc575c3feff518fe98712c66dc65a191
Jessime <jessime.kirk@gmail.com>
2017-08-06 02:01:02 -0400

    Simulation runs and wins/losses are updated.

--- App.py
+++ App.py
@@ -6,13 +6,15 @@ from flask import Flask, render_template, redirect, request, jsonify
 from flask import Flask, render_template, request, flash, send_from_directory
 from flaskext.mysql import MySQL
 
+from battle import Simulation
+
 app = Flask(__name__)
 
 ####################################### Sherif ###################################
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'sonoma'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Windows10'
 app.config['MYSQL_DATABASE_DB'] = 'pokemon_schema'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -26,6 +28,8 @@ class Data:
     stats = pd.read_csv('Pokemon.csv').groupby('#').first()
     username = None
     password = None
+    t1 = None
+    t2 = None
 
 def choose_teams():
     t1 = np.random.choice(range(1, 722), size=5, replace=False)
@@ -38,72 +42,51 @@ def choose_teams():
 def login():
     if request.method == 'POST':
         print('Login')
-        username = request.form['username']
-        password = request.form['password']
-        print username, password
-        cursor.execute("SELECT * from pokemon_table where username='" + username + "' and password='" + password + "'")
+        Data.username = request.form['username']
+        Data.password = request.form['password']
+        cursor.execute("SELECT * from pokemon_table where username='" + Data.username + "' and password='" + Data.password + "'")
         data = cursor.fetchone()
         if data is None:
             error = 'Invalid username or password. Please try again!'
             return render_template('login.html', error=error)
         else:
             #flash('You were successfully logged in')
-            t1, t2 = choose_teams()
-            return render_template('battle.html', t1=t1, t2=t2)
+            Data.t1, Data.t2 = choose_teams()
+            return render_template('battle.html', t1=Data.t1, t2=Data.t2)
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         print('Sign up')
-        username = request.form['username']
-        password = request.form['password']
+        Data.username = request.form['username']
+        Data.password = request.form['password']
 
-        print username, password
-        cursor.execute("SELECT * from pokemon_table where username='" + username + "'")
+        cursor.execute("SELECT * from pokemon_table where username='" + Data.username + "'")
         data = cursor.fetchone()
         if data is not None:
             error = ' username has already been taken!'
             return render_template('signup.html', error=error)
         else:
-            signUp(username, password)
-            t1, t2 = choose_teams()
-            return render_template('battle.html', t1=t1, t2=t2)
+            signUp(Data.username, Data.password)
+            Data.t1, Data.t2 = choose_teams()
+            return render_template('battle.html', t1=Data.t1, t2=Data.t2)
     return render_template('signup.html')
 
 @app.route('/battle', methods=['GET', 'POST'])
 def battle():
     if request.method == 'POST':
-        print('Sign up')
-    t1, t2 = choose_teams()
-    return render_template('battle.html', t1=t1, t2=t2)
+        guess = 0 if request.form['guess'] == 'team1' else 1
+        result = Simulation(Data.t1, Data.t2, guess)
+        updateTable(result, Data.username)
+    Data.t1, Data.t2 = choose_teams()
+    return render_template('battle.html', t1=Data.t1, t2=Data.t2)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
 #### ####################### Sherif #######################
-@app.route("/Authenticate", methods = ['POST', 'GET'])
-def Authenticate():
-    username = request.form['username']
-    password = request.form['password']
-    username="bonerChamps"
-    password="joe"
-    str="SELECT * from pokemon_table where username='" + username + "' and password='" + password + "'"
-    cursor.execute("SELECT * from pokemon_table where username='" + username + "' and password='" + password + "'")
-    data = cursor.fetchone()
-    print type(data),
-    if data is None:
-        error = 'Invalid username or password. Please try again!'
-
-        return render_template('home.html', error=error)
-    else:
-        error = 'You were successfully logged in'
-        return render_template('login.html', error=error)
-
-
 def signUp(username, password):
     query = "INSERT INTO pokemon_table (username, password,wins,losses) VALUES ('{}','{}',0,0)".format(username, password)
     cursor.execute(query)
