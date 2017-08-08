bbaa21166871c5bb7079a2c342bbaf01b0614196
Jessime <jessime.kirk@gmail.com>
2017-08-08 00:04:59 -0400

    Add Procfile
    Updated app.run() to be like LunchApp.

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 
@@ -113,6 +115,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    url = 'http://127.0.0.1:5001'
+    #url = 'http://127.0.0.1:5001'
     #open_new_tab(url)
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.debug = False
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
